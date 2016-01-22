4264334f9b39cc8b02d550e2c11e180905b46c9f
thabegginer <luis_anjos_@hotmail.com>
2016-01-22 19:37:58 +0000

    lets go again

--- pokedex-by-devslops/Pokemon.swift
+++ pokedex-by-devslops/Pokemon.swift
@@ -11,6 +11,13 @@ import Foundation
 class Pokemon {
     private var _name: String!
     private var _pokedexId: Int!
+    private var _description: String!
+    private var _type: String!
+    private var _defense: String!
+    private var _height: String!
+    private var _weight: String!
+    private var _attack: String!
+    private var _nextEvolution: String!
     
 
     var name: String {
@@ -19,6 +26,34 @@ class Pokemon {
     
     var pokedexId: Int {
         return _pokedexId
+    }
+    
+    var description: String {
+        return _description
+    }
+    
+    var type: String {
+        return _type
+    }
+    
+    var defense: String {
+        return _defense
+    }
+    
+    var height: String {
+        return _height
+    }
+    
+    var weight: String {
+        return _weight
+    }
+    
+    var attack: String {
+        return _weight
+    }
+    
+    var nextEvolution: String {
+        return _nextEvolution
     }
     
     init(name: String, pokedexId: Int) {
--- pokedex-by-devslops/PokemonDetailVC.swift
+++ pokedex-by-devslops/PokemonDetailVC.swift
@@ -13,6 +13,19 @@ class PokemonDetailVC: UIViewController {
     var pokemon: Pokemon!
 
     @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var mainImg: UIImageView!
+    @IBOutlet weak var descritionLbl: UILabel!
+    @IBOutlet weak var typeLbl: UILabel!
+    @IBOutlet weak var Defense: UILabel!
+    @IBOutlet weak var heightLbl: UILabel!
+    @IBOutlet weak var pokedexIdLbl: UILabel!
+    
+    @IBOutlet weak var currentEvoImg: UIImageView!
+    @IBOutlet weak var nextEvoImg: UIImageView!
+    @IBOutlet weak var evoLbl: UILabel!
+    
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -23,6 +36,12 @@ class PokemonDetailVC: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func backBtnPressed(sender: AnyObject) {
+        
+        dismissViewControllerAnimated(true, completion: nil)
+        
     }
     
 
